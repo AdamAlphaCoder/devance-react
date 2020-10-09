cf7b383f49165fcce4aafef7b28feae218115d54
Adam Peterson <adampeterson.work@gmail.com>
2020-10-10 00:53:07 +0800

    Add todo list and user list table UI

--- src/components/tabs/todos/index.js
+++ src/components/tabs/todos/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Button, Table } from 'react-bootstrap'
 
 // TODO:
 // * 1. LIST TODOS
@@ -7,10 +8,57 @@ import React from 'react'
 // * 4. UPDATE TODO
 // * 5. DELETE TODO
 
+// TODO: LIMIT BODY TO 25 CHARACTERS, SHOW FULL VERSION IN VIEW
+
 const Todos = () => {
   return (
     <div className="div mt-4">
-      <p>This is the tab for Todos</p>
+      <div className="d-flex justify-content-between mb-3">
+        <h3>Todo List</h3>
+        <Button variant="primary">Add Todo</Button>
+      </div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Body</th>
+            <th>User</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Lorem</td>
+            <td>Ipsum Dolor Sit Amet</td>
+            <td>Adam</td>
+            <td>
+              <Button variant="primary">View</Button>
+            </td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>How much</td>
+            <td>
+              Wood would a woodchuck chuck if a woodchuck could chuck wood
+            </td>
+            <td>Jason</td>
+            <td>
+              <Button variant="primary">View</Button>
+            </td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Mary had a</td>
+            <td>Little lamb and she was white as snow</td>
+            <td>Larry</td>
+            <td>
+              <Button variant="primary">View</Button>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
     </div>
   )
 }
--- src/components/tabs/users/index.js
+++ src/components/tabs/users/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { Button, Table } from 'react-bootstrap'
+
 // TODO:
 // * 1. LIST USERS
 // * 2. GET USER
@@ -10,7 +12,46 @@ import React from 'react'
 const Users = () => {
   return (
     <div className="div mt-4">
-      <p>This is the tab for Users</p>
+      <div className="d-flex justify-content-between mb-3">
+        <h3>User List</h3>
+        <Button variant="primary">Add User</Button>
+      </div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>User</th>
+            <th>Age</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>18</td>
+            <td>
+              <Button variant="primary">View</Button>
+            </td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>21</td>
+            <td>
+              <Button variant="primary">View</Button>
+            </td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Larry</td>
+            <td>50</td>
+            <td>
+              <Button variant="primary">View</Button>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
     </div>
   )
 }
